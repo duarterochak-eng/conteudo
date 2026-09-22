b0869aa1feadee2d1d1c4a44ac31c085aed1169e
duarterochak-eng <duarterochak@gmail.com>
2026-09-22 15:23:53 -0400

    Update next.config.mjs

--- conteudo/next.config.mjs
+++ conteudo/next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   serverExternalPackages: ["puppeteer-core", "@sparticuz/chromium"],
+  outputFileTracingIncludes: {
+    "/api/render": ["./node_modules/@sparticuz/chromium/**/*"],
+  },
 };
 export default nextConfig;
